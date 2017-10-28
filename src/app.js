939100baffd6c2a86cb8c8a494623b38d9a7c384
frontzhm <1143303516@qq.com>
2017-10-28 10:09:05 +0800

    基本的响应

--- src/app.js
+++ src/app.js
@@ -3,14 +3,40 @@ const http = require('http')
 const chalk = require('chalk')
 // 配置单独放一个文件
 const conf = require('./config/defaultConf')
+// 判断是不是文件
+const fs = require('fs')
 // 用户访问的地址变成服务器上的路径
 const path = require('path')
 
 const server = http.createServer((req, res) => {
   const filePath = path.join(conf.root, req.url)
-  res.statusCode = 200
-  res.setHeader('Content-Type','text-plain')
-  res.end(filePath)
+  // 一般用stat判断文件是不是存在
+  fs.stat(filePath, (err, stats) => {
+    if (err) {
+      res.statusCode = 404
+      res.setHeader('Content-type', 'text-plain')
+      res.end(`${filePath} is not a directory or file.`)
+      return
+    }
+    if (stats.isFile()) {
+      res.statusCode = 200
+      res.setHeader('Content-type','text-plain')
+      // 直接用流 流进res即可
+      fs.createReadStream(filePath).pipe(res)
+      return
+    }
+    if (stats.isDirectory()) {
+      // files是文件名的数组
+      fs.readdir(filePath, (err,files)=>{
+        res.statusCode = 200
+        res.setHeader('Content-type', 'text-plain')
+        console.log(files)
+        // 将文件名串起来
+        res.end(files.join(', '))
+      })
+    }
+
+  })
 })
 
 server.listen(conf.port, conf.hostname,()=>{
